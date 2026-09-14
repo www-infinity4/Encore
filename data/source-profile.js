99860854c2c3ea6af830369d8914a9a490da3066
www-infinity4 <ephraim.nwo@gmail.com>
2026-09-14 14:01:32 -0500

    Fix Cult Cinema source ID

--- data/source-profile.js
+++ data/source-profile.js
@@ -11,7 +11,7 @@ window.INFINITY_MOVIE_SOURCE={
   include:["full","movie","film","198","199","action","comedy","adventure","fantasy","thriller","drama"],
   exclude:["trailer","clip","short","review","reaction","2020","2021","2022","2023","2024","2025","2026"],
   playlists:[
-    {name:"Cult Cinema Classics uploads",id:"UUycDFnpMeWzaITQSD1dWsOA"},
+    {name:"Cult Cinema Classics uploads",id:"UUfHm4flY_lRnLvl5UA9HxJQ"},
     {name:"PizzaFlix uploads",id:"UUE34fwgW7kWr7tc7YiOWtRw"}
   ]
 };
